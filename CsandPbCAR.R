adce3855518cf59f9e9693e426e0fab9752d4254
HolmquistJ <HolmquistJ@si.edu>
2017-09-30 15:23:11 -0400

    Uploaded Pixel Probabilities for all Palustrine sub-classes.

--- CsandPbCAR.R
+++ CsandPbCAR.R
@@ -2,7 +2,8 @@
 # 26 September 2017
 car <- read.csv(paste(getwd(), "/data/MengReview/PbandcsData_170926.csv", sep=""))
 pb <- car$delSOC1[! is.na(car$delSOC1)]
-cs <- car$delSOC2[! is.na(car$delSOC2)]
+cs <- car$delSOC2[! is.na(car$delSOC2) ]
+cs <- cs[cs <= 1500]
 
 par(mfrow=c(1,2))
 
@@ -33,9 +34,23 @@ target_x <- seq(1, max(c(cs, pb)), by=5)
 target_y1 <- dnorm(log(target_x), log(median(cs)), sd(log(cs)))
 target_y2 <- dnorm(log(target_x), log(median(pb)), sd(log(pb)))
 
-
 points(log(target_x), target_y1, col="darkred", type="l", lwd=2)
 points(log(target_x), target_y2, col="darkblue", type="l", lwd=2)
 legend("topleft", c(expression(paste({}^137, "Cs")),
                     expression(paste({}^210, "Pb")))
                      , text.col=c("darkred", "darkblue"), bty="n")
+
+pb_se <- sd(log(pb)) / sqrt(length(pb))
+(exp(mean(log(pb))))
+(exp(mean(log(pb) + pb_se)))
+(exp(mean(log(pb) - pb_se)))
+
+# compounded uncertainty for area 10 +/- 1 pixel
+target_y3 <- dnorm(seq(0,20, by=0.1), 10, 1)
+plot(seq(0,20, by=0.1), target_y3, type="l", lwd=2)
+compounded_mean = log(10) + mean(log(pb))
+compounded_sd = sqrt((1 / 10) + (sd(log(pb))^2 / mean(log(pb))))
+
+(exp(compounded_mean))
+(exp(compounded_mean-compounded_sd))
+(exp(compounded_mean+compounded_sd))
